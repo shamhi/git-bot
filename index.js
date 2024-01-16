a16cfe6db81be3ce9e8987579a4428a29ea732dd
ShamHi <shomakakdela@gmail.com>
2024-01-16 18:47:59 +0300

    Initial commit

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const makeCommit = (n) => {
     if (n === 0) return simpleGit().push();
     const x = getRandomNumber(0, 56);
     const y = getRandomNumber(0, 6);
-    const DATE = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+    const DATE = moment().subtract(1, "y")
+        .add(1, "d").add(x, "w")
+        .add(y, "d")
+        .format();
 
     const data = {
         date: DATE
